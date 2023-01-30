27ec9d377386e8cb99d92fc4643cade27c39b253
Unnamed <geoffroymillon@gmail.com>
2023-01-30 10:48:39 +0100

    kikiramene

---
+++ Pages/Config.cshtml.cs
@@ -0,0 +1,78 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KiKiRamene.Pages
+{
+    public class ConfigModel : PageModel
+    {
+
+        public ConfigModel()
+        {
+        }
+
+        private string[] participants = new string[] {
+            "Damien",
+            "Omar",
+            "Hugues",
+            "Julien",
+            "Stephane",
+            "Arnaud",
+            "Fabien",
+            "Séverine",
+            "Romain",
+            "Geoffroy"
+        };
+
+        public DateTime Date { get; set; }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Config? Config { get; set; }
+
+        public IActionResult OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            if(Config?.Secret != "SECRET")
+            {
+                return Page();
+            }
+
+            if (Config != null)
+            {
+                try
+                {
+                    string path = "history.txt";
+
+                    using (StreamWriter sw = System.IO.File.Exists(path) ? System.IO.File.AppendText(path) : System.IO.File.CreateText(path))
+                    {
+                        sw.WriteLine(Config.Date + "-" + participants[new Random().Next(0, participants.Length)]);
+                    }
+                }
+                catch (IOException e)
+                {
+                    Console.WriteLine("The file could not be read:");
+                    Console.WriteLine(e.Message);
+                }
+
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+    public class Config
+    {
+        public DateTime Date { get; set; }
+
+        public string? Participant { get; set; }
+
+        public string? Secret { get; set; }
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KiKiRamene.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+            try
+            {
+                string path = "history.txt";
+
+                var text = System.IO.File.ReadLines(path);
+                var infos = text.LastOrDefault()?.Split("-");
+                ViewData["winner"] = infos[1];
+                ViewData["date"] = infos[0];
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
